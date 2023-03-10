dabbbdd2cef2d71e58e30381829ab8e507400528
Eva van Rooijen <evav.rooijen@live.nl>
2023-03-10 17:50:46 +0100

    add classes

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# find_schedule, update_preferences
 # functions to import, preprocess and visualize data
 import pandas as pd
 from dataclasses import dataclass
@@ -131,3 +132,94 @@ with open(file) as f:
             nurse_ID =  lines[idx+1].split(',')[0]
             print(nurse_ID)
             None
+
+# find_schedule: given instance and model solve NSP
+def find_schedule(instance):
+    # define MIP
+    # demo: have shifts to cover and set of nurses with different weekend preferences (check CAO!)
+    # by changing their weekend tolerance, show differences in schedule
+    # some like weekends: pay others do not: personal life
+
+    NSP = Model('NSP')
+    #NSP.context.cplex_parameters.mip.tolerances.mipgap = 0 # check if gap is always 0 ensured
+    NSP.set_time_limit(1*60) # in seconds CHECK
+    x = NSP.binary_var_matrix(len(N), len(S), name = "x")
+
+    obj_fn = sum(x.get((i, j)) for i in range(len(N)) for j in range(len(S)))
+    NSP.set_objective('max', obj_fn)
+
+    for j in range(len(S)):
+        # every shift should be covered by exactly one nurse (or at least? depends on definition of a shift)
+        NSP.add_constraint(sum(x.get((i, j)) for i in range(len(N))) == 1)
+        
+    for i in range(len(N)):
+        # TODO: every nurse has a contract with FTE
+        NSP.add_constraint(i.min_total_minutes < sum(j.length_in_min*x.get(i, j) for j in S) < i.max_total_minutes)
+        
+        # TODO: max weekends: count assigned shifts on day 5, 6, 12, 13, ... horizon
+        
+        # TODO: consecutiveness
+        
+        # TODO: max nr shifts
+        
+        
+        #NSP.add_constraint(0 < i.max_weekends) 
+    NSP.solve()
+    return NSP 
+
+file = r'D:\OneDrive - Ortec B.V\Thesis\Code\Code\NSP_benchmark\instances1_24\Instance1.txt'
+instance_1 = import_instance(file, 1)
+print(find_schedule(instance_1))
+    # gap = NSP.solve_details.mip_relative_gap
+    # #print(f"Optimal objective value z = {NSP.objective_value}")
+    #
+    # # visualize schedule, who works when
+    #
+    # # functions take solution as input and transform to readable schedule
+    # # get schedule_per_nurse(i)
+    # # get all shifts with nurse i
+    # outcome = []
+    # for v in NSP.iter_binary_vars():
+    #     outcome.append(int(v.solution_value))
+    #     #print(outcome)
+    # return NSP.objective_value, gap
+
+
+    # satisfaction is dan assigned_weekends/preference_weekends en maxmin
+
+    # objective: maxmin satisfaction + penalty unassigned shifts
+
+    # constraints: copied from benchmark
+
+    # max one shift per day
+
+    # min full day rest between shifts (forward rotation)
+
+    # cover requirements
+
+    # solve
+
+# def simulate_trades(Instance.S, Instance.N):
+#     # selling, buying, shift_ID, day
+#     return trades
+
+
+# update_preferences: given changes and nurse objects, update their preference parameters/profile
+def nurse_schedule_satisfaction(nurse, schedule):
+    # from schedule get consecutiveness, workload etc.
+
+    # flexibility, worklaod
+
+    satisfaction = None
+    return satisfaction
+
+def dept_schedule_satisfaction(dept_satisfactions):
+    return (min(dept_satisfactions)) # objective is maximization of min
+
+# def update_preferences(changes):
+#     for change in changes:
+#
+#     # create table with changes by nurse
+#
+#     for nurse in nurses_changed:
+#         train_satisfaction(nurse, changes[nurse])
